13483a0e39cb16f480aeec0cf38c30a25ac2c897
jurgenB123 <jurgenbaldacchino@gmail.com>
2017-02-25 11:18:43 +0100

    Created Random Weight Generator

--- Neural Network/Weight Initialization.cs
+++ Neural Network/Weight Initialization.cs
@@ -1,7 +1,17 @@
-﻿namespace Neural_Network
+﻿using System;
+
+namespace Neural_Network
 {
     public class Weight_Initialization
     {
-        //Testing GIT TOool ---- 
+        //Returns a random weight between -0.5 and 0.5 both inclusive 
+        public double GenerateRandomWeight()
+        {            
+            double maximum = 0.5;
+            double minimum = -0.5;
+            return new Random().NextDouble() * (maximum - minimum) + minimum;
+        }
+
+
     }
 }

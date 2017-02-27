f64b07d24e7e9d79aad164a01f30ed7f0206d673
jurgenB123 <jurgenbaldacchino@gmail.com>
2017-02-27 18:25:39 +0100

    Added Summation Functionality Added WriteToFile Functionality for Saving Data

--- Neural Network/Heartbeat.cs
+++ Neural Network/Heartbeat.cs
@@ -1,6 +1,20 @@
-﻿namespace Neural_Network
+﻿using System;
+namespace Neural_Network
 {
     class Heartbeat
     {
+        public const String PATH = @"C:\Users\user\Desktop\Running and Calories Thesis\Repositories\Neural Networks\Swimming Performance Neural\WriteTest.txt";
+        public static void Main()
+        {
+
+            Console.WriteLine("Starting Neural Network Heartbeat");
+            Console.WriteLine("Writing to File ...");
+
+            double[] test = { 1, 2, 3, 4, 5, 6 };
+            new WriteTextFile().writeToFile(PATH, test, "Blowing up this file ... ");
+
+            Console.WriteLine("Write Finished.");
+            Console.ReadLine();
+        }
     }
 }

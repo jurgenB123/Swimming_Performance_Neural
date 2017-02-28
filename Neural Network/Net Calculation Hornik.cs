a7f4437d0bc67e9008ad2bdb3fdf6d8a13b2ee50
jurgenB123 <jurgenbaldacchino@gmail.com>
2017-02-28 17:49:54 +0100

    Implemented Hornik et al. Method for Net function

---
+++ Neural Network/Net Calculation Hornik.cs
@@ -0,0 +1,20 @@
+﻿namespace Neural_Network
+{
+    class Net_Calculation_Hornik
+    {
+        /*
+        * r is the activation function
+        * k is the number of hidden units 
+        * vjl and wij are weights 
+        * Oiand Ol are polarized value (biases)
+        * u is the data vector
+        */
+        public double Net(double r, int k, double vjl, double wij, double Oi, double Ol, double[] u)
+        {
+            Sigma sigma = new Sigma();
+            double summation2 = r * (sigma.Summation(wij, k, u, Oi) - Ol);
+            return sigma.Summation(vjl, k, summation2);
+        }
+
+    }
+}
--- Neural Network/Sigma.cs
+++ Neural Network/Sigma.cs
@@ -22,10 +22,35 @@ namespace Neural_Network
             double sum = 0;
             for (int k = 0; k < N; k++)
             {
-                sum += Math.Pow((y_Estimate - y_Actual), 2); 
+                sum += Math.Pow((y_Estimate - y_Actual), 2);
             }
 
             return sum;
+        }
+
+        //Used in Hornik et al. Summation 1
+        public double Summation(double vjl, int k, double summation2)
+        {
+            double sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                sum += vjl * summation2;
+            }
+
+            return sum;
+        }
+
+        //Used in Hornik et al. Summation 2
+        public double Summation(double wij, double n, double[] u, double Oi)
+        {
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += wij * u[i] - Oi;
+            }
+
+            return sum;
+
         }
     }
 }

2ed58e67bae0dcb85082713de9cab2de86cbd929
jurgenB123 <jurgenbaldacchino@gmail.com>
2017-03-14 18:19:15 +0100

    Included Prerequisits for Fuzzy Logic Controller

---
+++ Fuzzy Logic/Heartbeat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fuzzy_Logic
+{
+    class Heartbeat
+    {
+
+        //Initialize Prerequisits
+        Rules rule = new Rules();
+        Fuzzifier fuzzy = new Fuzzifier();
+        Defuzzifier defuzz = new Defuzzifier();
+        Inference inference = new Inference();
+
+        public static void Main()
+        {
+            Console.WriteLine("Hey there ..");
+            Console.ReadLine();
+        }
+    }
+}
